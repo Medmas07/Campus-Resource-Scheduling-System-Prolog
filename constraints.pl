c8606daae4a366542f55219ffc6a9ea232b12586
Medmas07 <mohamed.masmoudi0712@gmail.com>
2026-04-30 22:27:58 +0100

    first steps finished

--- constraints.pl
+++ constraints.pl
@@ -1,6 +1,6 @@
 :- module(constraints, [
     generate_schedule/1,
-    assign_all_courses/2,
+    assign_all_courses/3,
     assign_sessions/4,
     valid_partial/1,
     no_room_conflict/1,
@@ -23,15 +23,13 @@
 ]).
 
 generate_schedule(Schedule) :-
-    assign_all_courses([], Schedule).
+    all_courses(Courses),
+    assign_all_courses(Courses, [], Schedule).
 
-assign_all_courses(Schedule, Schedule) :-
-    all_courses_assigned(Schedule).
-assign_all_courses(Partial, Schedule) :-
-    course_sessions(Course, _),
-    \+ assigned_course(Course, Partial),
+assign_all_courses([], Schedule, Schedule).
+assign_all_courses([Course | Rest], Partial, Schedule) :-
     assign_sessions(Course, 1, Partial, Updated),
-    assign_all_courses(Updated, Schedule).
+    assign_all_courses(Rest, Updated, Schedule).
 
 assign_sessions(Course, SessionIndex, Partial, Partial) :-
     course_sessions(Course, TotalSessions),
@@ -39,7 +37,14 @@ assign_sessions(Course, SessionIndex, Partial, Partial) :-
 assign_sessions(Course, SessionIndex, Partial, Schedule) :-
     course_sessions(Course, TotalSessions),
     SessionIndex =< TotalSessions,
-    room_capacity(Room, _),
+    course_equipment(Course, Equipment),
+    room_equipment(Room, Equipment),
+    course_group(Course, Group),
+    group_size_of(Group, GroupSize),
+    room_capacity(Room, RoomCapacity),
+    GroupSize =< RoomCapacity,
+    instructor_of(Instructor, Course),
+    instructor_available(Instructor, Time),
     timeslot(Time),
     Assignment = assign(Course, SessionIndex, Room, Time),
     valid_partial([Assignment | Partial]),
@@ -93,14 +98,10 @@ group_conflicts_with(assign(Course, _, _, Time), [assign(OtherCourse, _, _, Time
 group_conflicts_with(Assignment, [_ | Rest]) :-
     group_conflicts_with(Assignment, Rest).
 
-assigned_course(Course, [assign(Course, _, _, _) | _]).
-assigned_course(Course, [assign(OtherCourse, _, _, _) | Rest]) :-
-    Course \= OtherCourse,
-    assigned_course(Course, Rest).
+assigned_course(Course, Schedule) :-
+    course_sessions(Course, Total),
+    findall(SessionIndex, member(assign(Course, SessionIndex, _, _), Schedule), Sessions),
+    length(Sessions, Total).
 
-all_courses_assigned(Schedule) :-
-    \+ missing_course(Schedule).
-
-missing_course(Schedule) :-
-    course_sessions(Course, _),
-    \+ assigned_course(Course, Schedule).
+all_courses(Courses) :-
+    findall(Course, course_sessions(Course, _), Courses).
